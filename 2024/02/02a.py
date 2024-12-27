930c5d161956732befdf37d7100b1416899d7607
Russell Sutherland <russell.sutherland@utoronto.ca>
2024-12-27 10:55:57 -0500

    Streamlined working code

--- 2024/02/02a.py
+++ 2024/02/02a.py
@@ -4,6 +4,9 @@ from pathlib import Path
 from itertools import pairwise
 from rich import print
 
+Debug = False
+Debug = True
+
 def diffs(v):
     dfs = []
     for i,j in pairwise(v):
@@ -11,17 +14,16 @@ def diffs(v):
     return dfs
 
 def asc_or_desc(v):
-    all_neg = all(i <= 0 for i in v)
-    all_pos = all(i >= 0 for i in v)
+    all_neg = all(i < 0 for i in v)
+    all_pos = all(i > 0 for i in v)
     return all_neg or all_pos
 
 def within_bounds(v):
-    good = all(abs(i) < 4 and abs(i) > 0 for i in v)
-    return good
+    return all(abs(i) < 4 and abs(i) > 0 for i in v)
 
 
 def main():
-    inf = './02.reports'
+    inf = '02.reports'
     p = Path(inf)
     with p.open() as fd:
         lines = [line.rstrip() for line in fd]
@@ -32,8 +34,8 @@ def main():
             good = asc_or_desc(dfs) and within_bounds(dfs)
             if good:
                 cnt += 1
-                print(toks, dfs)
-                print()
+                if Debug:
+                    print('good:',toks, dfs)
         print(cnt)
 
 if __name__ == '__main__':
--- 2024/02/02b.py
+++ 2024/02/02b.py
@@ -4,46 +4,65 @@ from pathlib import Path
 from itertools import pairwise, dropwhile, filterfalse
 from rich import print
 
+
 def diffs(v):
     dfs = []
     for i,j in pairwise(v):
         dfs.append(int(j) - int(i))
     return dfs
 
-def one_out_of_bound(v):
-    v_prime = []
-    vprime = dropwhile(lambda x: abs(x) == 0 or abs(x) > 3, v)
-    if (len(v) - len(vprime)) == 1 and asc_or_desc(vprime):
-        return vprime
-    else:
-        return v
 
 def asc_or_desc(v):
-    all_neg = all(i <= 0 for i in v)
-    all_pos = all(i >= 0 for i in v)
-    return all_neg or all_pos
+    return is_asc(v) or is_desc(v)
+
 
 def within_bounds(v):
-    good = all(abs(i) < 4 and abs(i) > 0 for i in v)
-    return good
+    return all(abs(i) < 4 and abs(i) > 0 for i in v)
+
+def is_asc(v):
+    return all(i>0 for i in v)
+
+def is_desc(v):
+    return all(i<0 for i in v)
+    
+def is_safe(v):
+    return asc_or_desc(v) and within_bounds(v)
+
+def is_safe_save_one(v):
+    cnt = 0
+    for i in range(len(v)-2):
+       tmp = v.copy()
+       tmp.pop(i)
+       if is_safe(tmp):
+           cnt += 1
+    tmp = v.copy()
+    tmp.pop()
+    if is_safe(tmp):
+        cnt += 1
+    if cnt == 1:
+        return True
+    else:
+        return False
 
 
 def main():
-    inf = '02a.reports'
-    p = Path(inf)
-    with p.open() as fd:
+    fname = '02.reports'
+    cwd = Path.cwd()
+    print(cwd)
+    inf = Path(fname)
+    with inf.open() as fd:
         lines = [line.rstrip() for line in fd]
-        cnt = 0
+        cnt1 = 0
+        cnt2 = 0
         for line in lines:
             toks = line.split(' ')
             dfs = diffs(toks)
+            if is_safe(dfs):
+                cnt1 += 1
+            elif is_safe_save_one(dfs):
+                cnt2 += 1
+        print(cnt1, cnt2, cnt1 + cnt2)
 
-            good = asc_or_desc(dfs) and within_bounds(dfs)
-            if good:
-                cnt += 1
-                print(toks, dfs)
-                print()
-        print(cnt)
 
 if __name__ == '__main__':
     main()
